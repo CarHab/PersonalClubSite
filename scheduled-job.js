e8d9578c853a4e3bac9ae8882ffdb081126d9f48
CarHab <carlos.lima@praxisjr.com.br>
2019-07-11 16:54:37 -0300

    asd

---
+++ scheduled-job.js
@@ -0,0 +1,6 @@
+const axios = require('axios')
+
+axios
+  .get('https://us-central1-personalclub-52112.cloudfunctions.net/api/cronjob')
+  .then(res => console.log(res.data.message))
+  .catch(err => console.log(err))
