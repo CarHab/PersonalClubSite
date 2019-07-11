fed7a9a23406538811d2f3e1fee97092fbe24698
CarHab <carlos.lima@praxisjr.com.br>
2019-07-11 17:20:00 -0300

    sd

--- scheduled-job.js
+++ scheduled-job.js
@@ -3,4 +3,4 @@ const axios = require('axios')
 axios
   .get('https://us-central1-personalclub-52112.cloudfunctions.net/api/cronjob')
   .then(res => console.log(res.data.message))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err + ' Wrong day'))
