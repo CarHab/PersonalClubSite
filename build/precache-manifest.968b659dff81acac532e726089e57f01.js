25cc674c3077cb19fa579c67c3f393431c8186db
CarHab <carlos.lima@praxisjr.com.br>
2019-07-18 14:50:18 -0300

    asd

---
+++ build/precache-manifest.968b659dff81acac532e726089e57f01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8114d397aa8b9c247c947d30f7b83448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10158fb0ac0c1763f3ed",
+    "url": "/static/css/main.1f1322f3.chunk.css"
+  },
+  {
+    "revision": "b5fdd6d2d75a0a38e137",
+    "url": "/static/js/2.909cfee5.chunk.js"
+  },
+  {
+    "revision": "10158fb0ac0c1763f3ed",
+    "url": "/static/js/main.24e7f2fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cbeb83bb7cb1ca219d36dc50332a1ac4",
+    "url": "/static/media/addAlunoIcone.cbeb83bb.png"
+  },
+  {
+    "revision": "c91781c7e96a670a427bce63483cc088",
+    "url": "/static/media/alunosIcone.c91781c7.png"
+  },
+  {
+    "revision": "30885bdb9424c47cc036003b79900ec7",
+    "url": "/static/media/logo.30885bdb.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -16,124 +16,113 @@ import dadosDesempenho from './pages/dadosDesempenho'
 import blockAluno from './pages/blockAluno'
 import axios from 'axios'
 
-axios.defaults.baseURL =
-  'https://us-central1-personalclub-52112.cloudfunctions.net/api'
+axios.defaults.baseURL = 'https://us-central1-personalclub-23861.cloudfunctions.net/api'
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#eeeeee',
-      main: '#333',
-      dark: '#9e9e9e',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#800000',
-      main: '#f00',
-      dark: '#ff9696',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  snack: {
-    backgroundColor: 'green'
-  }
+	palette: {
+		primary: {
+			light: '#eeeeee',
+			main: '#333',
+			dark: '#9e9e9e',
+			contrastText: '#fff'
+		},
+		secondary: {
+			light: '#800000',
+			main: '#f00',
+			dark: '#ff9696',
+			contrastText: '#fff'
+		}
+	},
+	typography: {
+		useNextVariants: true
+	},
+	snack: {
+		backgroundColor: 'green'
+	}
 })
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      authenticated: false
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			authenticated: false
+		}
+	}
 
-  handleLogin = () => {
-    this.setState({
-      authenticated: true
-    })
-  }
+	handleLogin = () => {
+		this.setState({
+			authenticated: true
+		})
+	}
 
-  componentWillMount() {
-    const token = localStorage.FBidToken
-    if (token) {
-      const decodedToken = jwtDecode(token)
-      if (decodedToken.exp * 1000 < Date.now()) {
-        localStorage.removeItem('FBidToken')
-        this.setState({
-          authenticated: false
-        })
-        history.push('/')
-      } else {
-        this.setState({
-          authenticated: true
-        })
-      }
-    }
-  }
+	componentWillMount() {
+		const token = localStorage.FBidToken
+		if (token) {
+			const decodedToken = jwtDecode(token)
+			if (decodedToken.exp * 1000 < Date.now()) {
+				localStorage.removeItem('FBidToken')
+				this.setState({
+					authenticated: false
+				})
+				history.push('/')
+			} else {
+				this.setState({
+					authenticated: true
+				})
+			}
+		}
+	}
 
-  render() {
-    const { authenticated } = this.state
-    return (
-      <MuiThemeProvider theme={theme}>
-        <div className="App">
-          <Router history={history}>
-            <div className="container">
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => <Login {...props} login={this.handleLogin} />}
-                />
-                <AuthRoute
-                  exact
-                  path="/home"
-                  component={home}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/listaDeAlunos"
-                  component={listaDeAlunos}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/aluno"
-                  component={aluno}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/dadosDesempenho"
-                  component={dadosDesempenho}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/blockAluno"
-                  component={blockAluno}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/addAluno"
-                  component={addAluno}
-                  authenticated={authenticated}
-                />
-              </Switch>
-            </div>
-          </Router>
-        </div>
-      </MuiThemeProvider>
-    )
-  }
+	render() {
+		const { authenticated } = this.state
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div className='App'>
+					<Router history={history}>
+						<div className='container'>
+							<Switch>
+								<Route
+									exact
+									path='/'
+									render={props => <Login {...props} login={this.handleLogin} />}
+								/>
+								<AuthRoute exact path='/home' component={home} authenticated={authenticated} />
+								<AuthRoute
+									exact
+									path='/listaDeAlunos'
+									component={listaDeAlunos}
+									authenticated={authenticated}
+								/>
+								<AuthRoute exact path='/aluno' component={aluno} authenticated={authenticated} />
+								<AuthRoute
+									exact
+									path='/dadosDesempenho'
+									component={dadosDesempenho}
+									authenticated={authenticated}
+								/>
+								<AuthRoute
+									exact
+									path='/blockAluno'
+									component={blockAluno}
+									authenticated={authenticated}
+								/>
+								<AuthRoute
+									exact
+									path='/addAluno'
+									component={addAluno}
+									authenticated={authenticated}
+								/>
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</MuiThemeProvider>
+		)
+	}
 }
 
 App.props = {
-  classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired
 }
 
 export default App
